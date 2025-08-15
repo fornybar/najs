d0248c3018f61a49d12b2fbfd217200fa2df57ee
harvidsen <hakon.arvidsen@eviny.no>
2025-08-15 10:25:29 +0200

    fix: Put snowfall inputs in overlay

--- overlays/default/default.nix
+++ overlays/default/default.nix
@@ -4,7 +4,9 @@ final: prev: {
   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
     (python-final: python-prev: {
       nkeys = python-prev.pkgs.callPackage (inputs.self + "/packages/nkeys-py") { };
-      najs = python-prev.pkgs.callPackage (inputs.self + "/packages/najs") { };
+      najs = python-prev.pkgs.callPackage (inputs.self + "/packages/najs") {
+        inherit inputs;
+      };
     })
   ];
 }
