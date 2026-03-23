a9a5135d1646c97eea37797ba2c433c642323b3d
Mattias Matthiesen <mattias.matthiesen@eviny.no>
2026-03-23 09:00:06 +0100

    flake: Remove snowfall-lib
    Moving to standard Nix flake because snowfall-lib is largely
    unmaintained and has too much magic

--- flake.nix
+++ flake.nix
@@ -3,10 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    snowfall-lib = {
-      url = "github:snowfallorg/lib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    systems.url = "github:nix-systems/default";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -18,16 +15,43 @@
   };
 
   outputs =
-    {
-      snowfall-lib,
-      ...
-    }@inputs:
-    snowfall-lib.mkFlake {
-      inherit inputs;
-      src = ./.;
+    inputs@{ self, nixpkgs, ... }:
+    let
+      systems = import inputs.systems;
 
-      outputs-builder = channels: {
-        formatter = inputs.treefmt-nix.lib.mkWrapper channels.nixpkgs ./treefmt.nix;
-      };
+      mkPkgs =
+        system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ self.overlays.default ];
+        };
+
+      forAllSystems = function: nixpkgs.lib.genAttrs systems (system: function (mkPkgs system));
+
+      mkTreefmt = pkgs: inputs.treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+    in
+    {
+      overlays.default = import ./overlays/default { inherit inputs; };
+
+      packages = forAllSystems (pkgs: {
+        najs = pkgs.callPackage ./packages/najs { inherit inputs; };
+        nkeys-py = pkgs.callPackage ./packages/nkeys-py { };
+        default = self.packages.${pkgs.system}.najs;
+      });
+
+      checks = forAllSystems (pkgs: {
+        format = (mkTreefmt pkgs).config.build.check self;
+        najs-build = self.packages.${pkgs.system}.najs;
+      });
+
+      formatter = forAllSystems (pkgs: (mkTreefmt pkgs).config.build.wrapper);
+
+      devShells = forAllSystems (pkgs: {
+        default = import ./shells/default/default.nix {
+          inherit inputs pkgs;
+          system = pkgs.system;
+        };
+      });
     };
 }
