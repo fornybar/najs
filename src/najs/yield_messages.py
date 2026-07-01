508c878abb82355a695b1a87eb076e75a0036044
Tobias Soltvedt <tobias.soltvedt@gmail.com>
2026-07-01 14:21:33 +0200

    Add: Auto Ack

--- src/najs/yield_messages.py
+++ src/najs/yield_messages.py
@@ -9,10 +9,32 @@ from nats.js.api import AckPolicy
 
 logger = logging.getLogger(name=__name__)
 
+
+async def auto_ack(msgs: list[Msg], policy: AckPolicy) -> None:
+    match policy:
+        case AckPolicy.ALL if len(msgs) > 0:
+            try:
+                await msgs[-1].ack()
+            except nats.errors.TimeoutError as e:
+                logger.log(msg=e, level=logging.ERROR)
+                raise
+        case AckPolicy.EXPLICIT:
+            try:
+                async with asyncio.TaskGroup() as tg:
+                    for msg in msgs:
+                        tg.create_task(msg.ack())
+            except* nats.errors.TimeoutError as e:
+                logger.log(msg=e, level=logging.ERROR)
+                raise
+        case AckPolicy.NONE:
+            pass
+
+
 async def yield_messages(  # noqa: PLR0913
     sub: JetStreamContext.PullSubscription,
     batch: int,
     *,
+    enable_auto_ack: bool = False,
     run_forever: bool = False,
     nats_timeout: float | None = 5,
     heartbeat: float | None = 1,
@@ -27,6 +49,10 @@ async def yield_messages(  # noqa: PLR0913
                     heartbeat=heartbeat,
                 )
                 yield msgs
+
+                ack_policy = (await sub.consumer_info()).config.ack_policy
+                if enable_auto_ack and ack_policy is not None:
+                    await auto_ack(msgs, ack_policy)
 
             except nats.errors.TimeoutError:
                 if not run_forever:
